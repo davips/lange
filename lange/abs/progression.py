4356dd01bf6f296a33c4c3037ffbe0a4d26e09c3
davips <dpsabc@gmail.com>
2021-01-12 16:14:26 -0300

    Add notice

--- lange/abs/progression.py
+++ lange/abs/progression.py
@@ -1,3 +1,26 @@
+#  Copyright (c) 2021. Davi Pereira dos Santos
+#  This file is part of the lange project.
+#  Please respect the license - more about this in the section (*) below.
+#
+#  lange is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  lange is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with lange.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  (*) Removing authorship by any means, e.g. by distribution of derived
+#  works or verbatim, obfuscated, compiled or rewritten versions of any
+#  part of this work is a crime and is unethical regarding the effort and
+#  time spent here.
+#  Relevant employers or funding agencies will be notified accordingly.
+
 import decimal as dec
 import math
 from functools import cached_property
